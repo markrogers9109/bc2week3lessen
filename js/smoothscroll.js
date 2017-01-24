4c0f169c165851c06850fa182d62c7241beebf95
Mark <mark@automaticclients.com>
2017-01-23 16:12:36 -0800

    Added fonts section

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -3,7 +3,7 @@
 var $ = jQuery;
 
 $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
+    $('a[href*="#"]:not([href="#collapseOne"]):not([href="#collapseTwo"]):not([href="#collapseHelper"]):not([href="#collapseContColors"]):not([href="#collapseContBg"]):not([href="#collapseCloseIcon"]):not([href="#collapseCarets"]):not([href="#collapseQFloats"]):not([href="#collapseCBlock"]):not([href="#collapseClearfix"]):not([href="#collapseSHC"]):not([href="#collapseImgRep"])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
